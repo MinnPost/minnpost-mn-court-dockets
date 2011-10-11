282f9ea40d3838287b5dd70d33bb106711163382
Kevin Schaul <kevin.schaul@gmail.com>
2011-10-11 14:47:37 -0500

    Added search, fixed date formatting, fixed some styles

--- engine.js
+++ engine.js
@@ -1,28 +1,58 @@
 var url='https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minnesota_us_district_courts&query=SELECT%20*%0AFROM%20swdata%0AORDER%20BY%20Start%20DESC%0ALIMIT%2010';
 
 function formatDate(date) {
-    month = date.getMonth() + 1;
-    date = date.getDate();
-    hours = date.getHours();
+    var ampm = 'a.m.';
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hours = date.getHours();
+    if (hours > 11) {
+        if (hours > 12) {
+            hours = hours - 12;
+        }
+        ampm = 'p.m.';
+    }
     minutes = date.getMinutes();
-    return month + '/' + date + ' ' + hours + ':' + minutes;
+    if (minutes < 10) {
+        minutes = minutes + '0';
+    }
+    return month + '/' + day + ' ' + hours + ':' + minutes + ' ' + ampm;
 }
 
-$.getJSON(url + '&callback=?', function(data) {
-    myd = data;
-    var h = '<ul class="cd_dockets-list">';
-	for (i in data) {
-	    // JavaScript needs time in milliseconds
-	    var start = new Date(parseInt(data[i].Start) * 1000);
-	    var end = new Date(parseInt(data[i].End) * 1000);
-	    console.log(start);
-	    h += '<li class="cd_docket">';
-	    h += '<div class="cd_start cd_item">' + start + '</div>';
-	    h += '<div class="cd_end cd_item">' + end + '</div>';
-	    h += '<div class="cd_court cd_item">' + data[i].Court + '</div>';
-	    h += '<div class="cd_description cd_item">' + data[i].Description + '</div>';
-	    h += '</li>';
-	}
-	h += '</ul>';
-	$('#cd_dockets-div').html(h);
+function getData(limit, searchTerm) {
+    url = formatURL(limit, searchTerm);
+    $.getJSON(url + '&callback=?', function(data) {
+        myd = data;
+        var h = '<ul class="cd_dockets-list">';
+    	for (i in data) {
+    	    // JavaScript needs time in milliseconds
+    	    var start = new Date(parseInt(data[i].Start) * 1000);
+    	    var end = new Date(parseInt(data[i].End) * 1000);
+    	    h += '<li class="cd_docket">';
+    	    h += '<div class="cd_court cd_item">' + data[i].Court + '</div>';
+    	    h += '<div class="cd_time cd_item">' + formatDate(start) + ' to ' + formatDate(end) + '</div>';
+    	    h += '<div class="cd_description cd_item">' + data[i].Description + '</div>';
+    	    h += '</li>';
+    	}
+    	h += '</ul>';
+    	$('#cd_dockets-div').html(h);
+    });
+}
+
+function formatURL(limit, searchTerm) {
+    var baseUrl = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=minnesota_us_district_courts&query='
+    var url = 'SELECT * from swdata';
+    url += ' WHERE Description LIKE \'%' + searchTerm + '%\'';
+    url += ' ORDER BY Start DESC';
+    url += ' LIMIT ' + limit;
+    console.log(url);
+    return baseUrl + encodeURIComponent(url);
+}
+
+$(document).ready(function() {
+    getData(10, '');
+    $('#search').submit(function() {
+        var query = $('#search-text').val();
+        getData(10, query);
+        return false;
+    });
 });
